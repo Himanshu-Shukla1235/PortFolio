75acd766822fb25f4aa126e6356df9222228dfd3
Blurix23 <145251086+Blurix23@users.noreply.github.com>
2024-09-12 12:54:51 +0530

    comit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/PortFolio/',  // Set this to match your repo name
+  base: '/PortFolio/', // Ensure this matches your GitHub repo name
+  build: {
+    outDir: 'build', // Change this to use 'build' folder instead of 'dist'
+  },
 });
